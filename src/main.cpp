9467b75158da572ec2e336f46a42cc54906af811
MrMilhas <danielmrezende@yahoo.com.br>
2023-10-05 17:43:36 -0300

    Finalizando leitura das instâncias

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <vector>
 #include <cstring>
-#include<conio.h>
 #include<math.h>
 
 using namespace std;
@@ -40,74 +39,83 @@ float euclidian_dist(int x1, int y1, int x2, int y2){
  * @return Graph*   Retorna o grafo montado;
  */
 Graph *build_graph(ifstream &instance) {
-    string line = "";
-    int count_line = 0;
+    string line = "";    //~ Variável auxiliar para ler cada linha do arquivo;
+    int count_line = 0;  //~ Contador de linha do arquivo de entrada;
 
-    int n = 0;
-    int h = 0;
-    int d = 0;
-    int tMax = 0;
-    vector<float> td;
+    int n = 0;           //~ Número de vértices;
+    int h = 0;           //~ Número de hotéis extras;
+    int d = 0;           //~ Número de viagens;
+    int tMax = 0;        //~ Duração máxima do passeio;
+    vector<float> td;    //~ Vetor contendo a duração de cada viagem;
 
-    Graph *new_graph;
+    int x = 0;           //~ Posição X inicial dos nós;
+    int y = 0;           //~ Posição Y inicial dos nós;
+    int sI = 0;          //~ Pontuação inicial dos nós;
+    int id = 0;          //~ Id inicial dos nós;
 
+    int count = 0;       //~ Contador auxiliar para os hotéis extras;
+
+    Graph *new_graph = new Graph(n);    //~ Grafo que será montado;
+
+    //& Percorre o arquivo de entrada lendo os dados da instância e adicionando os nós no grafo;
     while(!instance.eof()){
         if(count_line == 0){
-            std::getline(instance, line);         
+            getline(instance, line);         
             std::stringstream ss(line);
             string aux = "";
 
-            std::getline(ss, aux, ' ');
+            getline(ss, aux, '	');
             n = stoi(aux);
-            std::getline(ss, aux, ' ');
+            getline(ss, aux, '	');
             h = stoi(aux);
-            std::getline(ss, aux, ' ');
+            getline(ss, aux, '\n');
             d = stoi(aux);
 
             count_line++;
         }
         else{
             if(count_line == 1){
-                instance >> tMax;
+                getline(instance, line);         
+                std::stringstream ss(line);
+                string aux = "";
+                
+                getline(ss, aux, '\n');
+                tMax = stoi(aux);
                 count_line++;
             }
             else{
                 if(count_line == 2){
-                    for(int i=0; i<d; i++){
-                        std::getline(instance, line);         
-                        std::stringstream ss(line);
-                        string aux = "";
-
-                        std::getline(ss, aux, ' ');
-                        td.push_back(stoi(aux));
-                        std::getline(ss, aux, ' ');
-                        td.push_back(stoi(aux));
+                    getline(instance, line);         
+                    std::stringstream ss(line);
+                    string aux = " ";
+                    float num;
+                    
+                    for(int k=0; k<d; k++){
+                        getline(ss, aux, '	');
+                        num = std::stof(aux);
+                        td.push_back(num);
                     }
                     count_line++;
                 }
                 else{
                     if(count_line == 3){
-                        std::getline(instance, line);         
+                        getline(instance, line);         
                         std::stringstream ss(line);
                         count_line++;
                     }
                     else{
-                        int x = 0;
-                        int y = 0;
-                        int sI = 0;
-                        int id = 0;
-                        new_graph = new Graph(n);
+                        new_graph->order = n;
 
                         if(count_line == 4){
-                            std::getline(instance, line);         
+                            getline(instance, line);         
                             std::stringstream ss(line);
                             string aux = "";
                             
-                            std::getline(ss, aux, ' ');
+                            getline(ss, aux, '	');
                             x = stoi(aux);
-                            std::getline(ss, aux, ' ');
+                            getline(ss, aux, '	');
                             y = stoi(aux);
-                            std::getline(ss, aux, ' ');
+                            getline(ss, aux, '\n');
                             sI = stoi(aux);
 
                             new_graph->insert_node(id, 'I', sI, x, y);
@@ -116,15 +124,15 @@ Graph *build_graph(ifstream &instance) {
                         }
                         else{
                             if(count_line == 5){
-                                std::getline(instance, line);         
+                                getline(instance, line);         
                                 std::stringstream ss(line);
                                 string aux = "";
                                 
-                                std::getline(ss, aux, ' ');
+                                getline(ss, aux, '	');
                                 x = stoi(aux);
-                                std::getline(ss, aux, ' ');
+                                getline(ss, aux, '	');
                                 y = stoi(aux);
-                                std::getline(ss, aux, ' ');
+                                getline(ss, aux, '\n');
                                 sI = stoi(aux);
 
                                 new_graph->insert_node(id, 'F', sI, x, y);
@@ -132,35 +140,38 @@ Graph *build_graph(ifstream &instance) {
                                 id++;
                             }
                             else{
-                                if(count_line < (h+5)){
-                                    std::getline(instance, line);         
+                                
+                                if(count < h){
+                                    getline(instance, line);         
                                     std::stringstream ss(line);
                                     string aux = "";
                                     
-                                    std::getline(ss, aux, ' ');
+                                    getline(ss, aux, '	');
                                     x = stoi(aux);
-                                    std::getline(ss, aux, ' ');
+                                    getline(ss, aux, '	');
                                     y = stoi(aux);
-                                    std::getline(ss, aux, ' ');
+                                    getline(ss, aux, '\n');
                                     sI = stoi(aux);
 
                                     new_graph->insert_node(id, 'H', sI, x, y);
                                     count_line++;
                                     id++;
+                                    count++;
                                 }
                                 else{
-                                    std::getline(instance, line);         
+                                    getline(instance, line);         
                                     std::stringstream ss(line);
                                     string aux = "";
-                                    
-                                    std::getline(ss, aux, ' ');
+
+                                    getline(ss, aux, '	');
                                     x = stoi(aux);
-                                    std::getline(ss, aux, ' ');
+                                    getline(ss, aux, '	');
                                     y = stoi(aux);
-                                    std::getline(ss, aux, ' ');
+                                    getline(ss, aux, '\n');
                                     sI = stoi(aux);
 
                                     new_graph->insert_node(id, 'N', sI, x, y);
+                                    
                                     count_line++;
                                     id++;
                                 }
@@ -172,17 +183,48 @@ Graph *build_graph(ifstream &instance) {
         }
     }
 
+    //& Percorre o grafo adicionando as arestas em cada nó (grafo completo);
     for(int i=0; i<new_graph->order; i++){
         for(int j=0; j<new_graph->order; j++){
             if(i!=j){
                 Node *node1 = new_graph->get_node(i);
                 Node *node2 = new_graph->get_node(j);
-
                 float euclidian = euclidian_dist(node1->pos_x, node1->pos_y, node2->pos_x, node2->pos_y);
                 new_graph->insert_edge(i, j, euclidian);
             }
         }
     }
 
+    //& Retorna o grafo construído;
     return new_graph;
+}
+
+int main(int argc, char const *argv[]) {
+    cout << "######################################### BEM VINDO #########################################" << endl;
+    cout << "_____________________________________________________________________________________________" << endl;
+
+    cout << "-> Abrindo arquivo de entrada..." << endl;
+
+    //& Abrindo arquivo da instância;
+    string instance_path(argv[1]);
+    ifstream instance_file;
+    instance_file.open(argv[1], ios::in);
+    
+    if(instance_file.is_open()){
+        cout << "-> Arquivo aberto com sucesso!" << endl;
+        cout << "____________________________________________________" << endl;
+
+        cout << "-> Iniciando construção do grafo..." << endl;
+        Graph *new_graph = build_graph(instance_file);
+
+        cout << "-> Grafo construído com sucesso!" << endl;
+        cout << "____________________________________________________" << endl;
+    }
+    else{
+        return 1;
+    }
+    cout << "_____________________________________________________________________________________________" << endl;
+    cout << "#############################################################################################" << endl;
+
+    return 0;
 }
