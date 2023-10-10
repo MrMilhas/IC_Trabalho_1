51d2c5778e4c037e6e275f66a18b20c5d1424d11
MrMilhas <danielmrezende@yahoo.com.br>
2023-10-10 11:04:23 -0300

    Novos ajustes no grafo

--- headers/Graph.h
+++ headers/Graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_H
 
 #include "Node.h"
+#include <vector>
 
 using namespace std;
 
@@ -10,21 +11,23 @@ class Graph {
     public:
         //* Atributes ---------------------------------------------------------------
         int order;         //~ Ordem do grafo (total de nós);
+        int h = 0;         //~ Número de hotéis extras;
+        int d = 0;         //~ Número de viagens;
+        int tMax = 0;      //~ Duração máxima do passeio;
+        vector<float> td;  //~ Vetor contendo a duração de cada viagem;
         
         Node *first_node;  //~ Primeiro nó do grafo;
         Node *last_node;   //~ Último nó do grafo;
 
         //* Functions ---------------------------------------------------------------
-        Graph(int order);  //~ Constructor;
-        ~Graph();          //~ Destructor;
+        Graph(int order, int h, int d, int tMax, vector<float> td);  //~ Constructor;
+        ~Graph();                                                    //~ Destructor;
 
         Node *get_node(int id);                                         //~ Retorna um nó pelo seu id;
         void insert_node(int id, char type, int points, int x, int y);  //~ Insere novo nó;
-        void insert_edge(int id, int target_id, float dist);          //~ Insere nova aresta;
+        void insert_edge(int id, int target_id, float dist);            //~ Insere nova aresta;
         void remove_node(int id);                                       //~ Remove nó pelo seu id;
         bool search_node(int id);                                       //~ Verifica se nó existe;
 };
-
-
 
 #endif
