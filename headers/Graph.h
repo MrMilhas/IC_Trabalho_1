d5ade3bae0ecc51542fce357d17e16aa1b405d9b
MrMilhas <danielmrezende@yahoo.com.br>
2023-12-04 16:17:12 -0300

    Modificando estruturas

--- headers/Graph.h
+++ headers/Graph.h
@@ -7,6 +7,21 @@
 
 using namespace std;
 
+/**
+ * @brief Estrutura da partícula;
+ * 
+ */
+struct particle {
+    vector<vector<int>> atual_position;        //& Posição atual da partícula;
+    vector<vector<int>> best_local_position;   //& Melhor posição da partícula;
+    vector<vector<int>> best_global_position;  //& Melhor posição global;
+    vector<vector<pair<int, int>>> speed;      //& Velocidade da partícula;
+
+    float points;              //& Pontuação atual da partícula;
+    float best_local_points;   //& Melhor pontuação da partícula;
+    float best_global_points;  //& Melhor pontuação global;
+};
+
 class Graph
 {
 
@@ -35,12 +50,20 @@ public:
     vector<int> createHotelsCandidates();                                                          //~ Cria conjunto de candidatos de hoteis;
     vector<pair<int, double>> updateCandidates(vector<pair<int, double>> *candidates, Node *node); //~ Atualiza conjunto de candidatos;
     vector<vector<int>> createNeighborhood(vector<int> solution, vector<pair<int, double>> cand, vector<int> hotels);
-    //vector<vector<int>> createNeighborhood(vector<int> solution, vector<int> hotels);
     float get_total_dist(vector<int> solution);
     float get_total_points(vector<int> solution);
     float get_ls_points(vector<int> solution);
+    
+    vector<particle> initialize_particles (int n_particles);
+    float calculate_points (particle p);
+    vector<vector<pair<int, int>>> subtract_positions (vector<vector<int>> p1, vector<vector<int>> p2);
+    vector<vector<pair<int, int>>> calculate_speed (particle p, float w, float c1, float c2);
+    vector<vector<int>> calculate_position (particle p);
+    
     vector<vector<int>> heuristic();                                                             //~ Função heurística;
-        vector<vector<int>> randomizedHeuristic(float alfa, int numIt, int seed);                    //~ Função heurística randomizada;
+    vector<vector<int>> randomizedHeuristic(float alfa, int numIt, int seed);                    //~ Função heurística randomizada;
+    vector<vector<int>> simple_grasp();                                                          //~ Grasp simples;
+    vector<vector<int>> particle_cloud (int max_it, int n_particles, float w, float c1, float c2);
 };
 
 #endif
